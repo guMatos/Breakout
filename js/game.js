f87ca43ba94902d168ce77217be1b4448f9119f6
Hevertontorres <hevertontorres3003@hotmail.com>
2018-11-14 21:25:48 -0200

    a

--- js/game.js
+++ js/game.js
@@ -1,31 +1,50 @@
+
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    maxSafeDistance: 200,
+    parent: 'colisao',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 100
+            }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
 var player;
 var ball;
 var block;
 
-class game extends Phaser.Scene 
+function preload()
 {
-    constructor()
-    {super ({key: 'game'});}
-    
-    preload()
-    {
-        this.load.image("player", "imagens/player.png");
-        this.load.image("ball", "imagens/ball.png");
-        this.load.image("block", "imagens/block.png");
-    }
-    
-    create()
-    {
-        console.log("Hello world!"); 
-        this.backgroundColor = blue;
-        player = this.physics.add.sprite(50, 80, 'player');
-        ball = this.physics.add.sprite(300, 150, 'ball');
-        
-        this.physics.add.collider(ball, player);
-    }
+    this.load.image("player", "imagens/player.png");
+    this.load.image("ball", "imagens/ball.png");
+    this.load.image("block", "imagens/block.png");
+}
 
-    update()
-    {
-        
-    }
+function create()
+{
+    console.log("Hello world!"); 
+    this.backgroundColor = blue;
+    player = this.physics.add.image(50, 80, 'player');
+    ball = this.physics.add.image(300, 150, 'ball');
+
+    this.physics.add.collider(ball, player);
+}
+
+function update()
+{
+
 }
