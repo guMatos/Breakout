3306ec2b687c6cf50350aa4cf80f15ffc6ee9a73
Gustavo Matos <toice385@gmail.com>
2018-11-14 20:10:09 -0200

    Inicial

---
+++ js/gameConfig.js
@@ -0,0 +1,22 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 600,
+    height: 300,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: {
+                y: 1000,
+            }
+        }
+    },
+    scene: [game]
+};
+
+var game = new Phaser.Game(config);
+    game.global = {
+        score: 0,
+        highScore: 0
+    }
+var widthGameSize = this.game.config.width;
+var heightGameSize = this.game.config.height;
