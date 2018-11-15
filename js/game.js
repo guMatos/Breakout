ca1d938d97726e7aa58d68f781f2dc4ab36986ed
Hevertontorres <hevertontorres3003@hotmail.com>
2018-11-15 15:31:08 -0200

    coments

--- js/game.js
+++ js/game.js
@@ -34,40 +34,43 @@ function preload()
 
 function create()
 {
+    //definindo que não tem colisão em baixo
     this.physics.world.checkCollision.down = false;
-    var criar = this;
     
-    block = this.add.group();  
-
+    //cria os blocos
     for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 4; j++) {
-            var block = criar.add.image(50 + i * 75, 40 + j * 50, 'block');
+            var block = this.add.image(50 + i * 75, 40 + j * 50, 'block');
             
-            criar.physics.world.enable(block);
+            this.physics.world.enable(block);
             block.body.setImmovable(true);
             blocks[pos++] = block;
         }
     }
     
+    //cria o player
     player = this.physics.add.image(400, 580, 'player');
     player.body.setCollideWorldBounds(true);
-    player.body.setBounce(1);
     player.body.setImmovable(true);
     
+    //cria a bola
     ball = this.physics.add.image(400, 300, 'ball');
     ball.body.velocity.x = 150;
     ball.body.velocity.y = 150;
     ball.body.bounce.setTo(1); 
     ball.body.collideWorldBounds = true;
     
+    //ativa o teclado
     cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update(time, delta)
 {
-    this.physics.world.collide(ball, player,    function () {} );
-    this.physics.world.collide(ball, blocks,    function () { blocks.kill(); } );
+    //dois primeiros parâmetros são o que se colidem, e depois é um metodo
+    this.physics.world.collide(ball, player, collideplayer);
+    this.physics.world.collide(ball, blocks, collideblock);
     
+    //movimentação do player
     if (cursors.left.isDown) {
         player.body.setVelocityX(-p_speed);
     } else if (cursors.right.isDown) {
@@ -75,4 +78,16 @@ function update(time, delta)
     } else {
         player.setVelocityX(0);
     } 
+}
+
+function collideplayer(ball, player)
+{
+    //definir o que acontece se colidir na esquerda, direita e meio
+    console.log("colidiu com o player");
+}
+
+function collideblock(ball, blocks)
+{
+    //definir que o block some
+    console.log("colidiu com o bloco");
 }
